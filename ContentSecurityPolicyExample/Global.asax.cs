c9c30cca43e6f88c91cd1bc5aec54b0babe2184c
Minh Tam <minhtam.seta@gmail.com>
2021-07-26 17:35:13 +0700

    Add nwebsec

--- ContentSecurityPolicyExample/Global.asax.cs
+++ ContentSecurityPolicyExample/Global.asax.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NWebsec.Csp;
+using NWebsec.Mvc.HttpHeaders;
+using NWebsec.Mvc.HttpHeaders.Csp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -16,6 +19,23 @@ namespace ContentSecurityPolicyExample
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new SetNoCacheHttpHeadersAttribute());
+            filters.Add(new XRobotsTagAttribute() { NoIndex = true, NoFollow = true });
+            filters.Add(new XContentTypeOptionsAttribute());
+            filters.Add(new XDownloadOptionsAttribute());
+            filters.Add(new XFrameOptionsAttribute());
+            filters.Add(new XXssProtectionAttribute());
+            //CSP
+            filters.Add(new CspAttribute());
+            filters.Add(new CspDefaultSrcAttribute { Self = true });
+            filters.Add(new CspScriptSrcAttribute { Self = true });
+            //CSPReportOnly
+            filters.Add(new CspReportOnlyAttribute());
+            filters.Add(new CspScriptSrcReportOnlyAttribute { None = true});
         }
     }
 }
